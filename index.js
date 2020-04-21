001a0a5f209704cf4c40df8aec0d30dca653bfd5
jeff_simon <jeffsbillinfo@gmail.com>
2020-04-20 19:38:29 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+function writeCards(array, input)
+{
+    let returnArray = []
+    for(let i = 0; i < array.length; i++)
+    {
+        returnArray.push(`Thank you, ${array[i]}, for the wonderful surprise gift!`)
+    }
+    return returnArray;
+}
+
+//console.log(writeCards(["Ada", "Brendan", "Ali"], "birthday"))
+
+function countDown(input)
+{
+    for(let i = input; i > -1; i--)
+    {
+        console.log(i)
+    }
+}
+
+countDown(10)
